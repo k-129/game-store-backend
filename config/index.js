60de3e7cf5844243aafe823e160466174516f730
Erik Knoef <erikknoef1997@hotmail.com>
2023-06-08 10:51:02 +0100

    deploy finished

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ const cookieParser = require("cookie-parser");
 // ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
-
+const FRONTEND_URL = process.env.ORIGIN
 // Middleware configuration
 module.exports = (app) => {
   // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
@@ -22,7 +22,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [process.env.ORIGIN],
+      origin: [FRONTEND_URL],
     })
   );
 
